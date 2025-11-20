e2becefb1bc1379234d5513c377fc45df468520b
Harutune Davis <hdavis2100@ip-172-31-44-180.us-east-2.compute.internal>
2025-11-20 17:56:59 -0600

    add register controller

---
+++ backend/app/Http/Controllers/Register.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+
+class Register extends Controller
+{
+    
+	public function register(Request $request)
+	{
+
+		$data = $request->validate([
+			'name' => 'required|string|max:255|unique:users,name',
+			'email' => 'email|unique:users,email',
+			'password' => 'required|string',
+			'role' => 'required|in:planner,seeker',
+		]);
+
+		$user = User::create([
+			'name' => $data['name'],
+			'email' => $data['email'],
+			'password' => Hash:make($data['password']),
+			'role' => $data['role'],
+		]);
+
+		return response()->json([
+			'message' => 'User registered successfully',
+			'user' => $user,
+		]);
+
+
+
+	
+}
