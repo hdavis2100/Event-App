8eefdd218194f3d150a89dad7fc8d3df77bcdf94
Harutune <d.harutune@wustl.edu>
2025-11-26 23:22:28 -0600

    fix search

--- frontend/src/components/EventsPage.jsx
+++ frontend/src/components/EventsPage.jsx
@@ -10,26 +10,26 @@ function EventsPage({user}) {
     const [events, setEvents] = useState([]);
     const [eventsLoading, setEventsLoading] = useState(true);
     const date = searchParams.get('date');
-    const description = searchParams.get('description');
+    const search = searchParams.get('search');
     let navigate = useNavigate();
     
     useEffect(() => {
-        getEvents({date, description}).then(data => {
+        getEvents({search, date}).then(data => {
             if (data.success){
                 setEvents(data.events);
                 setEventsLoading(false);
             }
         });
-    }, [date, description]);
+    }, [date, search]);
 
     function handleSearch(event) {
         event.preventDefault();
         let date = event.target.date.value;
-        let description = event.target.description.value;
+        let search = event.target.search.value;
         const params = new URLSearchParams();
        
         if (date) params.append('date', date);
-        if (description) params.append('description', description);
+        if (search) params.append('search', search);
         navigate(`/events?${params.toString()}`);
     }
 
@@ -43,7 +43,7 @@ function EventsPage({user}) {
                 </label>
                 <label>
                     Description:
-                    <input type="text" name="description" defaultValue={description} />
+                    <input type="text" name="search" defaultValue={search} />
                 </label>
                 <button type="submit">Search</button>
             </form>
